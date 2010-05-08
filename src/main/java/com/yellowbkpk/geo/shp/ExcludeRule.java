9007571e5fdd9d51827973d5c0ed34d59f96dee4
Ian Dees <ian.dees@gmail.com>
2010-05-07 21:11:35 -0500

    Adding some comments for the exclusion mechanics.

--- src/main/java/com/yellowbkpk/geo/shp/ExcludeRule.java
+++ src/main/java/com/yellowbkpk/geo/shp/ExcludeRule.java
@@ -17,16 +17,29 @@ public class ExcludeRule {
 
     public boolean allows(Primitive w) {
         if (type != w.getType()) {
-            return false;
+            // The rule should not exclude if the types are different
+            return true;
+        }
+        
+        if ("-".equals(this.key)) {
+            // Dash key means exclude tagless primitives
+            return w.hasTags();
         }
 
         String primitiveTagValue = w.getTagValue(key);
         boolean tagExists = primitiveTagValue != null;
-        if (!tagExists) {
-            return "".equals(this.key);
-        } else {
+        if (tagExists) {
+            // Exclude primitives that have the specified tag key. If the user
+            // puts a "*" in for the exclude key's value, then the primitive
+            // will be excluded simply for having that tag (regardless of
+            // value). Otherwise, the tag's key and value have to match what the
+            // user enters.
             return "*".equals(this.value)
-                    || this.value.equals(primitiveTagValue);
+                    || !this.value.equals(primitiveTagValue);
+        } else {
+            // Exclude when the primitive has the tag but the value is empty
+            // (and the user specified that it be empty).
+            return !"".equals(this.value);
         }
     }
     
